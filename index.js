0c039583c3d56a19e5aab20cd28d2a1d78a28197
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-18 21:22:31 +0200

    Rename package

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ updates((update) => {
   console.log('Application update available:', update)
 })
 
-const bridge = new Bridge({ mount: '/renderer/dist', waypoint: 'index.html' })
+const bridge = new Bridge({ mount: 'renderer/dist/', waypoint: 'index.html' })
 await bridge.ready()
 
 const runtime = new Runtime()
 const pipe = await runtime.start({ bridge })
 
 pipe.on('close', () => Pear.exit())
-
-pipe.write('hi')
 
 Pear.teardown(() => pipe.destroy())
